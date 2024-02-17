3b4c0e4e4a6f0a720524261fea12118705ddf3e3
Owaisali11 <aliowais1680@gmail.com>
2024-02-17 18:16:46 +0000

    Models Added

---
+++ CA.Domain/DTOs/AccountBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA.DTOs
+{
+    public class AccountBase
+    {
+        public int Id { get; set; } 
+    }
+}
